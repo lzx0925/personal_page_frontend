d5197d106533f9335ed752c2753c45b395275cb4
Zixin Li <zli@quantumphotonics.club>
2024-09-06 16:04:01 -0700

    wordle ios animation

--- src/components/game/wordle/SingleLine.js
+++ src/components/game/wordle/SingleLine.js
@@ -1,9 +1,45 @@
+// import "./style.css";
+// import { useEffect } from "react";
+
+// export default function SingleLine({ row, word, res }) {
+//   const getAnimation = (res, i) => {
+//     if (!res) return {};
+//     return {
+//       animation: `reverse-board-${
+//         res[i] === 1 ? "gn" : res[i] === -1 ? "gy" : "yw"
+//       } 1s forwards`,
+//     };
+//   };
+
+//   return (
+//     <div className="words" id={row}>
+//       {Array.from({ length: 5 }, (_, i) => (
+//         <div className="word" key={i} style={getAnimation(res, i)}>
+//           <div
+//             className="letter-text"
+//             style={res ? { animation: "reverse-letter 1s forwards" } : {}}
+//           >
+//             {word[i] || ""}
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 import "./style.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SingleLine({ row, word, res }) {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (res) {
+      setAnimate(true);
+    }
+  }, [res]);
+
   const getAnimation = (res, i) => {
-    if (!res) return {};
+    if (!res || !animate) return {};
     return {
       animation: `reverse-board-${
         res[i] === 1 ? "gn" : res[i] === -1 ? "gy" : "yw"
@@ -17,7 +53,7 @@ export default function SingleLine({ row, word, res }) {
         <div className="word" key={i} style={getAnimation(res, i)}>
           <div
             className="letter-text"
-            style={res ? { animation: "reverse-letter 1s forwards" } : {}}
+            style={animate ? { animation: "reverse-letter 1s forwards" } : {}}
           >
             {word[i] || ""}
           </div>
