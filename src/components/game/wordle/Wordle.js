d9fd504ace515941ba678d7072cc5fd8e0878fa6
Zixin Li <zli@quantumphotonics.club>
2024-09-06 16:23:04 -0700

    debug iphone

--- src/components/game/wordle/Wordle.js
+++ src/components/game/wordle/Wordle.js
@@ -40,8 +40,9 @@ export default function Wordle() {
           }
         }, 1000);
       } catch (error) {
-        
-        setDebugMessage(`出问题啦！！ ${error.message || error}`); // Debug message 5
+        setDebugMessage(
+          `出问题啦！！! ${error.message || JSON.stringify(error)}`
+        ); // Debug message 5
       }
       // const res = await check_wordle(words[curRow]);
       // const data = res.data;
