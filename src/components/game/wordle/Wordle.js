4d5406dc8bca86dbc1241057ce08e374df3f1d44
Zixin Li <zli@quantumphotonics.club>
2024-09-06 17:03:10 -0700

    debug iphone

--- src/components/game/wordle/Wordle.js
+++ src/components/game/wordle/Wordle.js
@@ -12,45 +12,19 @@ export default function Wordle() {
   const [summary, setSummary] = useState(null);
   const [res, setRes] = useState(["", "", "", "", "", ""]);
   const [keyboard, setKeyboard] = useState();
-  const [debugMessage, setDebugMessage] = useState("");
 
   async function handleKeyBoardClick(value) {
     const len = words[curRow].length;
 
     if (value === "ENTER" && len === 5) {
-      setDebugMessage("点成功了，发到后端拿data..."); // Debug message 1
-      try {
-        const res = await check_wordle(words[curRow]);
-        const data = res.data;
+      const res = await check_wordle(words[curRow]);
+      const data = res.data;
 
-        setDebugMessage("拿到data了,更改status..."); // Debug message 2
-
-        // Update result for the current row
-        setRes((prevRes) =>
-          prevRes.map((res, i) => (i === curRow ? data.result : res))
-        );
-        setKeyboard(data.keyboard);
-
-        setDebugMessage("更改成功，按理说该触发动画了.."); // Debug message 3
-
-        setTimeout(() => {
-          if (curRow < 5) {
-            setCurRow(curRow + 1);
-            setDebugMessage(`结束...有动画了吗？？`); // Debug message 4
-          }
-        }, 1000);
-      } catch (error) {
-        const errorMessage = error?.err || "An unknown error occurred";
-        setDebugMessage(`出问题啦~ ${errorMessage}`); // Debug message 5
-      }
-      // const res = await check_wordle(words[curRow]);
-      // const data = res.data;
-
-      // setRes((prevRes) =>
-      //   prevRes.map((res, i) => (i === curRow ? data.result : res))
-      // );
-      // setKeyboard(data.keyboard);
-      // setTimeout(() => (curRow < 5 ? setCurRow(curRow + 1) : null), 1000);
+      setRes((prevRes) =>
+        prevRes.map((res, i) => (i === curRow ? data.result : res))
+      );
+      setKeyboard(data.keyboard);
+      setTimeout(() => (curRow < 5 ? setCurRow(curRow + 1) : null), 1000);
     } else if (value === "BACKSPACE" && len > 0) {
       setWord((prevWords) =>
         prevWords.map((word, i) => (i === curRow ? word.slice(0, -1) : word))
@@ -79,12 +53,7 @@ export default function Wordle() {
           <SingleLine key={row} row={row} word={words[row]} res={res[row]} />
         ))}
       </div>
-      <div
-        className="debug-message"
-        style={{ marginTop: "20px", color: "red" }}
-      >
-        {debugMessage}
-      </div>
+
       <Keyboard handleLetterClick={handleKeyBoardClick} update={keyboard} />
       {summary && (
         <Summary
